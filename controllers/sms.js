1e33f0ab47263b3ec327ad928f682cb9fc02c789
2gofinancialservices <69549933+2gofinancialservices@users.noreply.github.com>
2020-08-13 16:00:59 +0300

    Update sms.js

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
 const credentials = {
-    apiKey: 'YOUR_API_KEY',         // use your sandbox app API key for development in the test environment
-    username: 'YOUR_USERNAME',      // use 'sandbox' for development in the test environment
+    apiKey: 'd8b41ead19b66ee6774a34db0595f2026f1c468e40fceac512b696a18a2c79fe',         
+    username: 'Companysms',    
 };
 const AfricasTalking = require('africastalking')(credentials);
 
 // Initialize a service e.g. SMS
 const sms = africastalking.SMS
-const phone = []
+const phone_number = [phone]
 
 // Use the service
 const options = {
-    to: phone,
+    to: phone_number,
     message: "Dear customer, thank you for being part of 2go financial services limited"
 }
 
