35fe27aaa13c2b0eb7852a3479190f4ccbac9938
Maliabu <60360658+Maliabu@users.noreply.github.com>
2020-08-24 13:02:03 +0300

    Update sms.js

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,26 +1,20 @@
 require('dotenv').config();
 
-const credentials = {
-    apiKey: 'd8b41ead19b66ee6774a34db0595f2026f1c468e40fceac512b696a18a2c79fe',         
-    username: 'Companysms',    
-};
-const AfricasTalking = require('africastalking')(credentials);
+const apiKey = process.env.YOUR_API_KEY;
+const username = process.env.YOUR_USERNAME;
 
-// Initialize a service e.g. SMS
-const sms = africastalking.SMS
-const phone_number = [phone]
-
-// Use the service
-const options = {
-    to: phone_number,
-    message: "Dear customer, thank you for being part of 2go financial services limited"
+const sendSms = (phone, message) => {
+  const AfricasTalking = require('africastalking')(apikey, username);
+  const sms = africastalking.SMS
+    .create({
+       body: message,
+       from: process.env.PHONE_NUMBER,
+       to: phone
+     })
+    .then(response => console.log(response)).catch(err=>{
+        console.log(err);
+    });
 }
 
-// Send message and capture the response or error
-sms.send(options)
-    .then( response => {
-        console.log(response);
-    })
-    .catch( error => {
-        console.log(error);
-    });
+module.exports = sendSms;
+
